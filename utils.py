b45b1a5c523b779cfe5cfdc26fdbb0a5271adb76
sajadmanesh <sina.sajadmanesh@gmail.com>
2021-08-19 19:21:33 +0200

    manual implemention of confidence_interval

--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ import functools
 import dgl
 import numpy as np
 import torch
-import seaborn as sns
 
 
 def seed_everything(seed):
@@ -16,9 +15,25 @@ def seed_everything(seed):
     torch.cuda.manual_seed_all(seed)
 
 
+def bootstrap(data, func=np.mean, n_boot=10000, seed=None):
+    n = len(data)
+    data = np.asarray(data)
+    rng = np.random.default_rng(seed)
+    integers = rng.integers
+    
+    boot_dist = []
+    for i in range(int(n_boot)):
+        resampler = integers(0, n, n, dtype=np.intp)  # intp is indexing dtype
+        sample = [data.take(resampler, axis=0)]
+        boot_dist.append(func(*sample))
+        
+    return np.array(boot_dist)
+
+
 def confidence_interval(data, func=np.mean, size=1000, ci=95, seed=12345):
-    bs_replicates = sns.algorithms.bootstrap(data, func=func, n_boot=size, seed=seed)
-    bounds = sns.utils.ci(bs_replicates, ci)
+    bs_replicates = bootstrap(data, func=func, n_boot=size, seed=seed)
+    p = 50 - ci / 2, 50 + ci / 2
+    bounds = np.nanpercentile(bs_replicates, p)
     return (bounds[1] - bounds[0]) / 2
 
 
