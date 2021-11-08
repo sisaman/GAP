c189e2efa2b74817da22f6fdff70db89ea9c53f9
sajadmanesh <sina.sajadmanesh@gmail.com>
2021-11-08 18:16:33 +0100

    rewrite jobs.py

--- jobs.py
+++ jobs.py
@@ -1,22 +1,35 @@
 import os
-import logging
 import subprocess
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from tabulate import tabulate
 from tqdm import tqdm
+from pathlib import Path
 
 
 class JobManager:
-    def __init__(self, args, cmd_generator=None):
+    def __init__(self, args):
         self.args = args
-        self.name = args.name
         self.command = args.command
-        self.jobs_dir = args.jobs_dir
-        self.cmd_generator = cmd_generator
+        self.file = args.file
+
+    @property
+    def path(self):
+        return os.path.realpath(self.file)
+
+    @property
+    def dir(self):
+        return os.path.dirname(self.path)
+
+    @property
+    def name(self):
+        return Path(self.path).stem
+
+    @property
+    def output_dir(self):
+        return os.path.join(self.dir, self.name)
 
     def run(self):
-        if self.command == 'create':
-            self.create()
-        elif self.command == 'submit':
+        if self.command == 'submit':
             self.submit()
         elif self.command == 'status':
             self.status()
@@ -25,19 +38,10 @@ class JobManager:
         elif self.command == 'exec':
             self.exec()
 
-    def create(self):
-        os.makedirs(self.jobs_dir, exist_ok=True)
-        run_cmds = self.cmd_generator(self.args)
-
-        with open(os.path.join(self.jobs_dir, f'{self.name}.jobs'), 'w') as file:
-            for run in tqdm(run_cmds):
-                file.write(run + '\n')
-
-        print('job file created:', os.path.join(self.jobs_dir, f'{self.name}.jobs'))
-
     def submit(self):
         window = 7500
-        num_cmds = sum(1 for _ in open(os.path.join(self.jobs_dir, f'{self.name}.jobs')))
+        num_cmds = sum(1 for _ in open(self.file))
+        os.makedirs(self.output_dir, exist_ok=True)
 
         for i in tqdm(range(0, num_cmds, window), desc='submitting jobs'):
             begin = i + 1
@@ -49,22 +53,22 @@ class JobManager:
                 f'#$ -P ai4media\n',
                 f'#$ -l pytorch,sgpu,gpumem=24\n',
                 f'#$ -t {begin}-{end}\n',
-                f'#$ -o {self.jobs_dir}\n',
-                f'#$ -e {self.jobs_dir}\n',
+                f'#$ -o {self.output_dir}\n',
+                f'#$ -e {self.output_dir}\n',
                 f'#$ -cwd\n',
                 f'#$ -V\n',
                 f'export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:10240\n',
-                f'python jobs.py -n {self.name} exec --id $SGE_TASK_ID \n'
+                f'python jobs.py -f {self.file} exec --id $SGE_TASK_ID \n'
             ]
 
-            file_name = os.path.join(self.jobs_dir, f'{self.name}-{begin}-{end}.job')
+            job_file = os.path.join(self.output_dir, f'{self.name}-{begin}-{end}.job')
 
-            with open(file_name, 'w') as file:
+            with open(job_file, 'w') as file:
                 file.writelines(job_file_content)
                 file.flush()
 
             try:
-                subprocess.check_call(['qsub', file_name], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                subprocess.check_call(['qsub', job_file], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 print('\n\n', e.output, '\n\n')
                 raise e
@@ -75,32 +79,25 @@ class JobManager:
         failed_jobs = self.get_failed_jobs()
 
         if len(failed_jobs):
-            with open(os.path.join(self.jobs_dir, f'{self.name}.jobs')) as jobs_file:
+            with open(self.file) as jobs_file:
                 job_list = jobs_file.read().splitlines()
+            
+            run_cmds = [job_list[i - 1] for i, _, _ in failed_jobs]
 
-            self.cmd_generator = lambda _: [job_list[i - 1] for i, _, _ in failed_jobs]
-            self.name = f'{self.name}-resubmit'
-            self.create()
+            with open(self.args.new_file, 'w') as file:
+                for run in tqdm(run_cmds):
+                    file.write(run + '\n')
+
+            print('new job file created:', self.args.new_file)
+            self.file = self.args.new_file
             self.submit()
 
     def status(self):
-        try:
-            import tabulate
-        except ImportError:
-            tabulate = None
-
         failed_jobs = self.get_failed_jobs()
-
-        if tabulate:
-            print(tabulate.tabulate(failed_jobs, headers=['job id', 'error file', 'num lines']))
-        else:
-            for _, file, num_lines in failed_jobs:
-                print(num_lines, os.path.join(self.jobs_dir, file))
-
-        print()
+        print(tabulate(failed_jobs, headers=['job id', 'error file', 'num lines']) + '\n')
 
     def exec(self):
-        with open(os.path.join(self.jobs_dir, f'{self.name}.jobs')) as jobs_file:
+        with open(self.file) as jobs_file:
             job_list = jobs_file.read().splitlines()
 
         if self.args.all:
@@ -110,12 +107,12 @@ class JobManager:
             subprocess.check_call(job_list[self.args.id - 1].split())
 
     def get_failed_jobs(self):
-        failed_jobs = []
         file_list = [
-            os.path.join(self.jobs_dir, file)
-            for file in os.listdir(self.jobs_dir) if file.startswith(self.name) and file.count('.e')
+            os.path.join(self.output_dir, file)
+            for file in os.listdir(self.output_dir) if file.count('.e')
         ]
 
+        failed_jobs = []
         for file in file_list:
             num_lines = sum(1 for _ in open(file))
             if num_lines > 0:
@@ -125,26 +122,26 @@ class JobManager:
         return failed_jobs
 
     @staticmethod
-    def register_arguments(parser, default_jobs_dir='./jobs'):
-        parser.add_argument('-n', '--name', type=str, required=True, help='experiment name')
-        parser.add_argument('-j', '--jobs-dir', type=str, default=default_jobs_dir, help='jobs directory')
+    def register_arguments(parser):
+        parser.add_argument('-f', '--file', type=str, required=True, help='jobs file name')
         command_subparser = parser.add_subparsers(dest='command')
 
-        parser_create = command_subparser.add_parser('create')
         command_subparser.add_parser('submit')
         command_subparser.add_parser('status')
-        command_subparser.add_parser('resubmit')
+
+        parser_resubmit = command_subparser.add_parser('resubmit')
+        parser_resubmit.add_argument('-n', '--new-file', type=str, help='name of new jobs file', required=True)
 
         parser_exec = command_subparser.add_parser('exec')
         parser_exec.add_argument('--id', type=int)
         parser_exec.add_argument('--all', action='store_true')
 
-        return parser, parser_create
+        return parser
 
 
 def main():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser, parser_create = JobManager.register_arguments(parser)
+    parser = JobManager.register_arguments(parser)
     args = parser.parse_args()
     print(args)
 
