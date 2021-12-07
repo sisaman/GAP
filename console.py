38974942e70137204e05c37230d8d98843611ed3
sajadmanesh <sina.sajadmanesh@gmail.com>
2021-12-07 08:52:19 +0100

    send tracebacks to stderr

--- console.py
+++ console.py
@@ -6,13 +6,14 @@ import warnings
 import logging
 
 console = Console(tab_size=4)
-install(console=console, width=console.width)
+error_console = Console(stderr=True, tab_size=4)
+install(console=error_console, width=error_console.width)
 warnings.filterwarnings('ignore')
 logging.basicConfig(
     level='INFO', 
     format='%(message)s', 
     datefmt="[%X]", 
-    handlers=[RichHandler(rich_tracebacks=True, console=console, omit_repeated_times=True)]
+    handlers=[RichHandler(console=console, omit_repeated_times=True)]
 )
 
 def log(msg):
