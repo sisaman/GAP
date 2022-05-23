43ff973cf276fdfe11b58ff6a98ccdb57979a717
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-05-23 14:48:21 +0200

    fix issue with the amazon dataset

--- pysrc/datasets/amazon.py
+++ pysrc/datasets/amazon.py
@@ -4,7 +4,8 @@ from ogb.nodeproppred import PygNodePropPredDataset
 
 class Amazon(PygNodePropPredDataset):
     def __init__(self, root, transform=None, pre_transform=None):
-        super(Amazon, self).__init__(root, name='ogbn-products', transform=None, pre_transform=pre_transform)
+        super().__init__(root=root, name='ogbn-products', transform=None, pre_transform=pre_transform)
+        self.data.__num_nodes__ = self.data.x.size(0)
         self.data.y = self.data.y.view(-1)
         split_idx = self.get_idx_split()
         for split, idx in split_idx.items():
