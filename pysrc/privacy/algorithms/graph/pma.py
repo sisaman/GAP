9f3e4e194e0ef9f97eb60dde19e3c8f27c250b21
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-05-29 17:21:14 +0200

    PMA only perturbs aggregations

--- pysrc/privacy/algorithms/graph/pma.py
+++ pysrc/privacy/algorithms/graph/pma.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn.functional as F
-from torch_geometric.data import Data
 from pysrc.privacy.mechanisms.commons import GaussianMechanism, InfMechanism, ZeroMechanism
 from pysrc.privacy.mechanisms.composed import ComposedGaussianMechanism
 from pysrc.privacy.mechanisms.noisy import NoisyMechanism
@@ -27,21 +25,5 @@ class PMA(NoisyMechanism):
 
         self.set_all_representation(mech)
 
-    def __call__(self, data: Data, sensitivity: float) -> Data:
-        assert hasattr(data, 'adj_t')
-
-        x = F.normalize(data.x, p=2, dim=-1)
-        x_list = [x]
-
-        for _ in range(1, self.params['hops'] + 1):
-            # aggregate
-            x = data.adj_t.matmul(x)
-            # perturb
-            x = self.gm(x, sensitivity=sensitivity)
-            # normalize
-            x = x = F.normalize(x, p=2, dim=-1)
-
-            x_list.append(x)
-
-        data.x = torch.stack(x_list, dim=-1)
-        return data
+    def __call__(self, x: torch.Tensor, sensitivity: float) -> torch.Tensor:
+        return self.gm(x, sensitivity=sensitivity)
