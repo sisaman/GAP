37ca447b6216bcafe601d5790cb60ebbd7b959a7
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-05-29 17:19:53 +0200

    MethodBase accepts num_classes

--- pysrc/methods/base.py
+++ pysrc/methods/base.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 from torch_geometric.data import Data
 
 class MethodBase(ABC):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+
     @abstractmethod
     def reset_parameters(self) -> None: pass
 
