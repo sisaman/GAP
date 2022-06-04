7e23297c2af75d90d158e8749ee1697730387617
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-06-04 20:24:38 +0200

    update console

--- pysrc/console/__init__.py
+++ pysrc/console/__init__.py
@@ -5,18 +5,22 @@ from rich.logging import RichHandler
 from pysrc.console.console import Console
 
 
-def setup() -> Console:
+def setup(log_level: str='INFO', ignore_warnings: bool=True) -> Console:
     console = Console(tab_size=4)
     error_console = Console(stderr=True, tab_size=4)
     install(console=error_console, width=error_console.width)
-    warnings.filterwarnings('ignore')
+    if ignore_warnings:
+        warnings.simplefilter('ignore')
     logging.basicConfig(
-        level='INFO', 
+        level=log_level,
         format='%(message)s', 
         datefmt="[%X]", 
-        handlers=[RichHandler(console=console, omit_repeated_times=True)]
+        handlers=[RichHandler(
+            console=console, 
+            omit_repeated_times=True,
+        )]
     )
     return console
 
 
-console = setup()
+console = setup(log_level='DEBUG', ignore_warnings=False)
