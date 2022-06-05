1c420f96f9578a7aebf247ac16b26b1208be44db
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-06-05 15:17:59 +0200

    optimize calibrator

--- pysrc/privacy/mechanisms/noisy.py
+++ pysrc/privacy/mechanisms/noisy.py
@@ -11,14 +11,6 @@ class NoisyMechanism(Mechanism):
         self.name = 'NoisyMechanism'
         self.params = {'noise_scale': noise_scale}
 
-    def is_zero(self) -> bool:
-        eps = np.array([self.RenyiDP(alpha) for alpha in range(2,100)])
-        return np.all(eps == 0.0)
-
-    def is_inf(self) -> bool:
-        eps = np.array([self.RenyiDP(alpha) for alpha in range(2,100)])
-        return np.all(eps == np.inf)
-
     def update(self, noise_scale: float) -> Self:
         self.params.pop('noise_scale')
         self.__init__(noise_scale, **self.params)
@@ -28,20 +20,21 @@ class NoisyMechanism(Mechanism):
         if self.params['noise_scale'] == 0:
             self.update(noise_scale=1)  # to avoid is_inf being true
 
-        if eps == np.inf or self.is_inf() or self.is_zero():
+        if np.isinf(eps):
             self.update(noise_scale=0)
             return 0.0
         else:
-            fn_err = lambda x: abs(eps - self.update(x).get_approxDP(delta))
+            fn_err = lambda x: abs(eps - self.update(np.exp(x)).get_approxDP(delta))
 
             # check if the mechanism is already calibrated
             if fn_err(self.params['noise_scale']) < 1e-3:
                 return self.params['noise_scale']
-
-            results = minimize_scalar(fn_err, method='bounded', bounds=[0,1000], tol=1e-8, options={'maxiter': 1000000})
+            
+            results = minimize_scalar(fn_err, method='brent', options={'xtol': 1e-5, 'maxiter': 1000})
 
             if results.success and results.fun < 1e-3:
-                self.update(results.x)
-                return results.x
+                noise_scale = np.exp(results.x)
+                self.update(noise_scale)
+                return noise_scale
             else:
-                raise RuntimeError(f"eps_delta_calibrator fails to find a parameter:\n{results}")
+                raise RuntimeError(f"calibrator failed to find noise scale\n{results}")
