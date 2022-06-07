57ab22f25dad9e6ea92821a718f54e1c7cc07d67
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-06-07 14:16:24 +0200

    add val_interval option to MethodBase

--- pysrc/methods/base.py
+++ pysrc/methods/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Literal, Optional
+from typing import Annotated, Literal, Optional
 from torch import Tensor
 from torch_geometric.data import Data
 from pysrc.classifiers.base import Metrics
@@ -7,14 +7,21 @@ from pysrc.trainer import Trainer
 
 
 class MethodBase(ABC):
-    def __init__(self, num_classes: int, device:  Literal['cpu', 'cuda'] = 'cuda', use_amp: bool = False):
+    def __init__(self, 
+                 num_classes: int, 
+                 device:  Literal['cpu', 'cuda'] = 'cuda', 
+                 use_amp: bool = False,
+                 val_interval: Annotated[int, dict(help='interval of validation')] = 1,
+                 ):
         self.num_classes = num_classes
         self.device = device
         self.use_amp = use_amp
 
         self.trainer = Trainer(
+            val_interval=val_interval,
             use_amp=self.use_amp, 
-            monitor='val/acc', monitor_mode='max', 
+            monitor='val/acc', 
+            monitor_mode='max', 
             device=self.device,
         )
 
