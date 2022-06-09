9bed2930519981948bdc346601e1f862e154ed1a
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-06-09 19:27:26 +0200

    MAJOR: replace inbound+outbound degree sampling by outbound-only

--- pysrc/data/transforms/neighbor_sampler.py
+++ pysrc/data/transforms/neighbor_sampler.py
@@ -1,37 +1,26 @@
 import torch
 from torch_geometric.transforms import BaseTransform
 from torch_geometric.data import Data
-from torch_sparse import SparseTensor
-import torch.utils.cpp_extension
+from torch_geometric.loader.utils import to_csc, filter_data
 
 
 class NeighborSampler(BaseTransform):
     def __init__(self, max_degree: int):
-        self.max_deg = max_degree
-        
-        try:
-            edge_sampler = torch.ops.my_ops.sample_edge
-        except RuntimeError:
-            torch.utils.cpp_extension.load(
-                name="sampler",
-                sources=['csrc/sampler.cpp'],
-                build_directory='csrc',
-                is_python_module=False,
-                verbose=False,
-            )
-            edge_sampler = torch.ops.my_ops.sample_edge
-        
-        self.edge_sampler = edge_sampler
+        self.num_neighbors = max_degree
+        self.with_replacement = False
 
     def __call__(self, data: Data) -> Data:
-        N = data.num_nodes
-        E = data.num_edges
-        adj: SparseTensor = data.adj_t.t()
-        device = adj.device()
-        row, col, _ = adj.coo()
-        perm = torch.randperm(E)
-        row, col = row[perm], col[perm]
-        row, col = self.edge_sampler(row.tolist(), col.tolist(), N, self.max_deg)
-        adj = SparseTensor(row=row, col=col).to(device)
-        data.adj_t = adj.t()
+        data.adj_t = data.adj_t.t()
+        data = self.sample(data)
+        data.adj_t = data.adj_t.t()
+        return data
+
+    def sample(self, data: Data) -> Data:
+        colptr, row, perm = to_csc(data, device='cpu')
+        index = torch.range(0, data.num_nodes-1, dtype=int)
+        sample_fn = torch.ops.torch_sparse.neighbor_sample
+        node, row, col, edge = sample_fn(
+            colptr, row, index, [self.num_neighbors], self.with_replacement, True
+        )
+        data = filter_data(data, node, row, col, edge, perm)
         return data
